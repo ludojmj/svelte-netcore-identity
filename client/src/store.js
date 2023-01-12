6307e9dcd2fdb572fbcaa8040f268c69bfd2dc22
ludo <ludovic.jarno@axa.fr>
2023-01-13 00:12:50 +0100

    subscribe/unsubscribe

--- client/src/store.js
+++ client/src/store.js
@@ -6,3 +6,4 @@ export const selectedItem = writable(JSON.parse(localStorage.getItem("selectedIt
 selectedItem.subscribe(val => localStorage.setItem("selectedItem", JSON.stringify(val)));
 
 export const tokens = writable(null);
+tokens.subscribe(value => value);
