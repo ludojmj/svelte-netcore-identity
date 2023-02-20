66463579fed1bc6280c7322e3f9a80d1b32a1df3
ludojmj <ludovic.villejarno@gmail.com>
2023-02-20 17:22:59 +0100

    add tokens info

--- client/src/components/oidc/AuthWrapper.svelte
+++ client/src/components/oidc/AuthWrapper.svelte
@@ -1,6 +1,6 @@
 <script>
   // AuthWrapper.svelte
-  import { isAuthLoading, tokens } from "../../lib/store.js";
+  import { isAuthLoading, isLoading, tokens } from "../../lib/store.js";
   import { getToken } from "../../lib/oidc.js";
   import Loading from "../common/Loading.svelte";
   import Login from "./Login.svelte";
@@ -14,6 +14,9 @@
     <Loading />
   {:else if $tokens}
     <slot />
+    {#if !$isLoading}
+      <pre>{JSON.stringify($tokens, null, "\t")}</pre>
+    {/if}
   {:else}
     <div class="alert alert-warning">
       You need to login to access this site.
