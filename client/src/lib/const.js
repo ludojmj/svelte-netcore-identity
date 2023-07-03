c48583a3cdf8f5c12b3f80fe869099758b676342
ludojmj <ludovic.villejarno@gmail.com>
2023-07-03 10:34:22 +0200

    refresh_time_before_tokens_expiration_in_second

--- client/src/lib/const.js
+++ client/src/lib/const.js
@@ -5,6 +5,7 @@ export const configuration = {
   silent_redirect_uri: window.location.origin + "/authentication/silent-callback",
   scope: "openid profile email api offline_access",
   authority: "https://demo.duendesoftware.com",
+  refresh_time_before_tokens_expiration_in_second: 40,
   service_worker_relative_url: '/OidcServiceWorker.js',
   service_worker_only: false,
   // silent_login_timeout: 1000,
